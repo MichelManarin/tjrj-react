f5db0015e3d6005be8bb96a7c5b2fc6b02d8a5ee
Michel Manarin <michelmanarin@gmail.com>
2024-08-05 16:47:49 -0300

    fix: build

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://tjrj-book-test-rest-api-f4h9f9cycmbgfrdc.eastus-01.azurewebsites.net/v1/",
+  baseURL: "https://tjrj-book-test-rest-api-f4h9f9cycmbgfrdc.eastus-01.azurewebsites.net/",
 });
 
 export default api;
